5daaf08f1c284c843ac7b4bacbfbcbce5f1a7855
6849010008-sumet <6849010008@phuketvc.ac.th>
2026-08-08 14:03:50 +0700

    step6

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -5,55 +5,17 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //return Row(
     return Column(
-      //mainAxisAlignment: MainAxisAlignment.start,
-      //mainAxisAlignment: MainAxisAlignment.center,
-      //mainAxisAlignment: MainAxisAlignment.end,
-      //mainAxisAlignment: MainAxisAlignment.spaceAround,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Container(
-          color: Colors.redAccent,
-          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          child: const Text(
-            'hello world',
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 3,
-            ),
-          ),
-        ),
-        Container(
-          color: Colors.greenAccent,
-          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          child: const Text(
-            'hello world',
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 3,
-            ),
-          ),
-        ),
-         Container(
-          color: Colors.blueAccent,
-          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          child: const Text(
-            'hello world',
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 3,
-            ),
-          ),
-        ),
-      ],
+        Image.asset('assets/images/desy1.jpg',width: 200,height: 200),
+          SizedBox(height: 20),
+          Image.network('https://img.kapook.com/u/2020/Jarosphan/Home/Garden/91812/00.jpg',
+          width: 200,
+          height: 200,
+        )
+      ]
     );
   }
 }
