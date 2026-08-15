40b35cde787e8d14820b590f9d2cda74275fae2c
6849010008-sumet <6849010008@phuketvc.ac.th>
2026-08-15 11:40:41 +0700

    step11

--- lib/model/person.dart
+++ lib/model/person.dart
@@ -1,16 +1,62 @@
+import 'package:flutter/material.dart';
+
+enum Job{
+  developer(
+    title: "นักพัฒนา",
+    description: "ทำงานเกี่ยวกับการพัฒนาโปรแกรมและแอปพลิเคชัน",
+    image: "assets/images/desy1.png",
+    color: Colors.purpleAccent
+  ),
+  designer(
+    title: "นักออกแบบ",
+    description: "ทำงานเกี่ยวกับการออกแบบกราฟิกและอินเทอร์เฟซผู้ใช้",
+    image: "assets/images/desy2.png",
+    color: Colors.blueAccent
+  ),
+  manager(
+    title: "ผู้จัดการ",
+    description: "ทำงานเกี่ยวกับการบริหารจัดการและการวางแผน",
+    image: "assets/images/desy3.png",
+    color: Colors.orangeAccent
+  ),
+  analyst(
+    title: "นักวิเคราะห์",
+    description: "ทำงานเกี่ยวกับการวิเคราะห์ข้อมูลและการวางแผนธุรกิจ",
+    image: "assets/images/desy4.png",
+    color: Colors.yellowAccent
+  ),
+  marketer(
+    title: "นักการตลาด",
+    description: "ทำงานเกี่ยวกับการวางแผนและการดำเนินกลยุทธ์การตลาด",
+    image: "assets/images/desy5.png",
+    color: Colors.redAccent
+  );
+
+  const Job({
+    required this.title,
+    required this.description,
+    required this.image,
+    required this.color,
+  });
+
+  final String title;
+  final String description;
+  final String image;
+  final Color color;
+}
 class Person {
   String name;
   int age;
-  String job;
+  Job job;
 
   //Constructor
   Person({required this.name, required this.age, required this.job});
 }
 
 List<Person> people = [
-  Person(name: "สมชาย", age: 30, job: "นักพัฒนา"),
-  Person(name: "สมหญิง", age: 25, job: "นักออกแบบ"),
-  Person(name: "สมปอง", age: 35, job: "ผู้จัดการ"),
-  Person(name: "สมศรี", age: 28, job: "นักวิเคราะห์"),
-  Person(name: "สมจิต", age: 32, job: "นักการตลาด"),
+  Person(name: "สมชาย", age: 30, job: Job.developer),
+  Person(name: "สมหญิง", age: 25, job: Job.designer),
+  Person(name: "สมปอง", age: 35, job: Job.manager),
+  Person(name: "สมศรี", age: 28, job: Job.analyst),
+  Person(name: "สมจิต", age: 32, job: Job.marketer),
 ];
--- lib/screen/item.dart
+++ lib/screen/item.dart
@@ -16,7 +16,8 @@ class _ItemState extends State<Item> {
       itemBuilder: (context, index) {
         return Container(
           decoration: BoxDecoration(
-            border: Border.all(color: const Color.fromARGB(255, 243, 33, 138), width: 2),
+            // border: Border.all(color: Colors.lightGreen),
+            color: people[index].job.color,
             borderRadius: BorderRadius.circular(10),
           ),
           margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
@@ -38,10 +39,16 @@ class _ItemState extends State<Item> {
                   ),
                   SizedBox(height: 5),
                   Text(
-                    'อาชีพ: ${people[index].job}',
+                    'อาชีพ: ${people[index].job.title}',
                     style: TextStyle(fontSize: 16),
                   ),
                 ],
+              ),
+              Image.asset(
+                people[index].job.image,
+                width: 80,
+                height: 80,
+                fit: BoxFit.cover,
               ),
             ]
           ),
