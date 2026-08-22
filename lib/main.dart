303d8c355fe6d73143e5786ddc74d9bb03cda427
6849010008-sumet <6849010008@phuketvc.ac.th>
2026-08-22 13:45:40 +0700

    step17

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 // import 'screen/home.dart';
-import 'package:learn_flutter_68_1/screen/home.dart';
-
-import 'package:learn_flutter_68_1/screen/addForm.dart';
+// import 'package:learn_flutter_68_1/screen/home.dart';
+import 'package:learn_flutter_68_1/screen/item.dart';
+// import 'package:learn_flutter_68_1/screen/addFrom.dart';
 
 void main() {
   // runApp(const MyApp());
@@ -10,19 +10,40 @@ void main() {
   // const app = MaterialApp(title: 'my app', home: Text('hello world'));
   // runApp(app);
 
-  //   runApp(
-  //     MaterialApp(
-  //       title: 'my app',
-  //       home: Scaffold(
-  //         appBar: AppBar(
-  //           title: Text('my app'),
-  //           backgroundColor: Colors.red,
-  //           centerTitle: true,
-  //         ),
-  //         // body: Center(child: Text('hello world')),
-  //         body: Home(),
+  // runApp(
+  //   MaterialApp(
+  //     title: 'my app',
+  //     home: Scaffold(
+  //       appBar: AppBar(
+  //         title: Text('my app'),
+  //         backgroundColor: const Color.fromARGB(255, 58, 160, 255),
+  //         centerTitle: true,
   //       ),
+  //       // body: Center(child: Text('hello world')),
+  //       body: Item(),
   //     ),
-  //   );
-  runApp(addform());
+  //   ),
+  // );
+
+  // runApp(AddForm());
+  runApp(MyApp());
 }
+
+  class MyApp extends StatelessWidget {
+    const MyApp({super.key});
+
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        title: "Add Person",
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Add Person"),
+            backgroundColor: Colors.blueAccent,
+            centerTitle: true,
+          ),
+          body: Item(),
+        ),
+      );
+    }
+  }
