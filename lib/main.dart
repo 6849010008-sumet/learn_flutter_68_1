59553788bbdaad1498c0289cfa48f435bd8513d1
6849010008-sumet <6849010008@phuketvc.ac.th>
2026-08-22 11:37:19 +0700

    step15

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,27 @@ import 'package:flutter/material.dart';
 // import 'screen/home.dart';
 import 'package:learn_flutter_68_1/screen/home.dart';
 
+import 'package:learn_flutter_68_1/screen/addForm.dart';
+
 void main() {
   // runApp(const MyApp());
 
   // const app = MaterialApp(title: 'my app', home: Text('hello world'));
   // runApp(app);
 
-  runApp(
-    MaterialApp(
-      title: 'my app',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('my app'),
-          backgroundColor: Colors.red,
-          centerTitle: true,
-        ),
-        // body: Center(child: Text('hello world')),
-        body: Home(),
-      ),
-    ),
-  );
+  //   runApp(
+  //     MaterialApp(
+  //       title: 'my app',
+  //       home: Scaffold(
+  //         appBar: AppBar(
+  //           title: Text('my app'),
+  //           backgroundColor: Colors.red,
+  //           centerTitle: true,
+  //         ),
+  //         // body: Center(child: Text('hello world')),
+  //         body: Home(),
+  //       ),
+  //     ),
+  //   );
+  runApp(addform());
 }
